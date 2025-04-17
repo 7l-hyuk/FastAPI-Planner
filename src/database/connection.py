3e86862cf1aae4874f201ee450a21f56fcd73606
Gihyuk Kwon <imrlgur81@gmail.com>
2025-04-17 13:59:58 +0900

    feat: add database CRUD

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,30 +1,22 @@
-from pydantic import ConfigDict
-from pydantic_settings import BaseSettings
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie, Document
 
-from src.logger import logger
+from src.utils.logger import logger
 from src.models.events import Event
 from src.models.users import User
+from src.config import settings
 
 
-class Settings(BaseSettings):
-    DATABASE_URL: str | None = None
-
-    async def initialize_database(self):
-        try:
-            client = AsyncIOMotorClient(self.DATABASE_URL)  # Client Access
-            await init_beanie(
-                database=client.get_default_database(),
-                document_models=[Event, User]
-            )
-            logger.info("database connection success")
-        except Exception as e:
-            logger.info(f"{e}")
-
-    model_config = ConfigDict(
-        env_file='.env'
-    )
+async def initialize_database():
+    try:
+        client = AsyncIOMotorClient(settings.DATABASE_URL)  # Client Access
+        await init_beanie(
+            database=client.get_default_database(),
+            document_models=[Event, User]
+        )
+        logger.info("DATABASE CONNECTION SUCCESS")
+    except Exception as e:
+        logger.info(f"ERROR: {e}")
 
 
 class Database:
@@ -34,10 +26,19 @@ class Database:
     async def save(self, document: Document):
         await document.create()
         return
-    
+
     async def get_all(self):
         docs = await self.model.find_all().to_list()
         return docs
-    
 
-    
+    async def get(self, id: int):
+        doc = await self.model.get(id)
+        return doc
+
+    async def delete(self, id: int):
+        doc = await self.get(id)
+        await doc.delete()
+
+    async def update(self, id: int, body):
+        doc = await self.get(id)
+        await doc.update(body)
--- src/routes/events.py
+++ src/routes/events.py
@@ -1,46 +1,40 @@
 from fastapi import APIRouter
 from src.models.events import Event
+from src.database.connection import Database
 
 event_router = APIRouter(
     prefix="/events",
     tags=["Events"]
 )
 
-events: list[Event] = []
+event_db = Database(Event)
 
 
 @event_router.get("/", response_model=list[Event])
 async def get_all_events():
+    events = await event_db.get_all()
     return events
 
 
-@event_router.get("/{id}", response_model=Event)
-def get_event(id: int):
-    for event in events:
-        if event.id == id:
-            return event
-    return {"error": "Event not found"}
+@event_router.get("/{id}")
+async def get_event(id: int):
+    event = await event_db.get(id)
+    return event
 
 
 @event_router.post("/")
 async def create_event(event: Event):
-    events.append(event)
+    await event_db.save(event)
     return {"message": "Event created successfully"}
 
 
 @event_router.put("/{id}")
 async def update_event(id: int, event: Event):
-    for i, event in enumerate(events):
-        if event.id == id:
-            events[i] = event
-            return {"message": "Event updated successfully"}
-    return {"message": "Event not found"}
+    await event_db.update(id, event)
+    return {"message": "Event updated successfully"}
 
 
 @event_router.delete("/{id}")
 async def delete_event(id: int):
-    for event in events:
-        if event.id == id:
-            events.remove(event)
-            return {"message": "Event deleted successfully"}
-    return {"message": "Event not found"}
+    await event_db.delete(id)
+    return {"message": "Event deleted successfully"}
